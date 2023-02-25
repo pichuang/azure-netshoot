019ddd22bffbef577a33410713eae746f6f6600d
Phil Huang <phil.huang@microsoft.com>
2023-02-25 10:37:40 +0800

    update

---
+++ READMD.md
@@ -0,0 +1,50 @@
+# Exporting Route Tables
+
+This bash script allows you to export all of the route tables associated with an Azure virtual hub to individual JSON files. The script uses the Azure CLI to retrieve the route table IDs and associated information and then calls the get-effective-routes command to export the routes to JSON.
+
+## Usage
+
+1. Clone or download the repository containing the script.
+2. Open a terminal and navigate to the directory containing the script.
+3. Modify the variables at the top of the script as necessary:
+
+   - `RESOURCE_GROUPNAME`: The name of the resource group containing the virtual hub.
+   - `VHUB_NAME`: The name of the virtual hub.
+   - `DEBUG_OUTPUT`: Set this to `true` or `false` to output debugging information.
+   - `TIMEZONE`: Set this to your local timezone.
+
+4. Run the script using the following command:
+
+``` bash
+$ bash export_route_tables.sh
+```
+
+5. The script will create a directory with the format `VHUB_NAME-YYYYMMDD` and save each route table to a separate JSON file in that directory.
+
+## Demo
+
+```
+phil [ ~ ]$ ./get-effective-routes.sh
+Directory vhub-cc-20230225 already exists
+Save defaultRouteTable-20230225-022901.json
+Save noneRouteTable-20230225-022901.json
+
+phil [ ~ ]$ ls
+exporting-route-tables-from-vhub.sh vhub-cc-20230225
+
+phil [ ~ ]$ ls -la vhub-cc-20230225/
+total 32
+drwxr-xr-x 2 phil phil 4096 Feb 25 02:29 .
+drwxr-xr-x 6 phil phil 4096 Feb 25 02:22 ..
+-rw-r--r-- 1 phil phil 1090 Feb 25 02:29 defaultRouteTable-20230225-022901.json
+-rw-r--r-- 1 phil phil   18 Feb 25 02:29 noneRouteTable-20230225-022901.json
+```
+
+
+## Requirements
+
+Make sure that you have installed the Azure CLI and jq command-line JSON processor before running the script.
+
+- Azure CLI
+- `jq` command-line JSON processor
+
---
+++ exporting-route-tables-from-vhub.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Specify the resource group and vhub names
+RESOURCE_GROUPNAME="rg-pinhuang"
+VHUB_NAME="vhub-cc"
+TIMEZONE="Asia/Taipei"
+DEBUG_OUTPUT=false # true or false
+
+# Get the current timestamp
+timestamp="$(TZ=${TIMEZONE} date +%Y%m%d-%H%M%S)"
+
+# Get the resource ID
+resource_id=$(az network vhub list --resource-group ${RESOURCE_GROUPNAME} --output tsv --query '[].id')
+if [ $? -ne 0 ]; then
+    echo "Failed to get resource ID"
+    exit 1
+else
+    if $DEBUG_OUTPUT; then
+        echo "vHub Resource ID: $resource_id"
+    fi
+    dir_name="${VHUB_NAME}-$(date +%Y%m%d)"
+
+    # if the directory doesn't exist, create it
+    if [ ! -d $dir_name ]; then
+        mkdir -p $dir_name
+        echo "Create directory $dir_name"
+    else
+        echo "Directory $dir_name already exists"
+    fi
+fi
+
+# Get the list of route table IDs
+route_tables=$(az network vhub route-table list --resource-group ${RESOURCE_GROUPNAME} --vhub-name ${VHUB_NAME} --output json --query "[].{name: name, id: id}" | jq -c ".[]")
+if [ $? -ne 0 ]; then
+    echo "Failed to get route table IDs"
+    exit 1
+fi
+
+# Loop over the route table IDs and get the effective routes for each
+for rt in $route_tables; do
+    # Get the route table name and ID
+    rt_name=$(echo $rt | jq -r '.name')
+    rt_id=$(echo $rt | jq -r '.id')
+
+    if $DEBUG_OUTPUT; then
+        echo "Route table name: $rt_name"
+        echo "Route table ID: $rt_id"
+    fi
+
+    # Create the output filename
+    filename="${rt_name}-${timestamp}.json"
+
+    # Call the get-effective-routes command and output the results in JSON format
+    az network vhub get-effective-routes --resource-type RouteTable --resource-id ${rt_id} --resource-group ${RESOURCE_GROUPNAME} --name ${VHUB_NAME} --output json > $dir_name/$filename
+
+    # Check the status of the previous command
+    if [ $? -ne 0 ]; then
+        echo "Failed to get effective routes for route table $rt_name"
+        exit 1
+    else
+        echo "Save ${filename}"
+    fi
+done
+# End of script
